d2d9f4228a3b23ef332bde945c93a710b98faa9e
Natthaphon Suphansri <Natthaphon.s2018@gmail.com>
2023-10-24 03:34:32 +0700

    add claims to all method header

--- api-gateway/example-api.js
+++ api-gateway/example-api.js
@@ -4,6 +4,10 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req,res, next) => {
+    console.log(req.headers)
+    next()
+})
 
 const users = ["user1"];
 const secretKey = "hehe"; // Replace with your own secret key
